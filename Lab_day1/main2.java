744a570485a2259b9afb4acfeaa328721692f6ac
Ayush Srivastava <ayushsriavstava@icloud.com>
2024-11-07 11:27:52 +0530

    Delete Lab_day1 directory

--- Lab_day1/main2.java
+++
@@ -1,8 +0,0 @@
-public class main2 {
-
-    public static void main(String[] args) {
-        String x=args[0];
-        String y=args[1];
-        System.out.println("University Name: "+x+" "+y );
-    }
-}
